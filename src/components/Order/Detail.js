c04e54a15724561853a222d26e4dff262824f6cc
So2diE <so2die@hotmail.com>
2018-08-09 11:35:15 +0800

    styleed order page

--- src/components/Order/Detail.js
+++ src/components/Order/Detail.js
@@ -2,10 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import * as  styleGuide from '../../constants/styleGuide'
-import {Divider, Grid, ListItem, ListItemIcon, ListItemText} from '@material-ui/core'
+import {Grid, ListItem} from '@material-ui/core'
 
 import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 
@@ -15,7 +14,50 @@ const styles = theme => ({
         fontWeight: 'bold',
         color: styleGuide.purpleText,
 
+
+    },
+    items: {
+        borderBottom: '1px solid' + styleGuide.greyDivider,
+        paddingBottom: '30px',
+        marginBottom: '30px',
+    },
+    shippingDetails: {
+        borderLeft: '1px solid' + styleGuide.greyDivider,
+    },
+    contactDetail: {},
+    detailHeader: {
+        display:'inline-block',
+        paddingBottom:'20px',
+        fontSize: '18px',
+        fontFamily: 'ocsSemiBold',
+
+    },
+    detailIcon: {
+        fontSize: '30px',
+        paddingRight: '20px',
+    },
+    detailContent: {
+        fontFamily: 'ocsSemiBold',
+        fontSize: '14px',
+
+    },
+    nested: {
+        padding: '30px',
+    },
+    detailPadding:{
+        paddingBottom:'10px',
+
+    },
+    rightChild:{
+        paddingLeft: '40px',
+
+    },
+    leftParent:{
+        paddingRight:'40px',
     }
+
+
+
 });
 const items = [
     {
@@ -61,115 +103,181 @@ class OrderDetail extends React.Component {
         const {classes} = this.props;
 
         return (
-            <div >
+            <div>
                 <ListItem button onClick={this.handleClick}>
                     <Grid container alignItems={'center'}>
-                   <Grid item sm={2}>
-                       #190209-1B
-                   </Grid>
-                    <Grid item sm={2}>
-                    Success
-                       as of 10/17/18
-                   </Grid>
-                    <Grid item sm={2}>
-                       Tom cfsdaofo
-                   </Grid>
-                    <Grid item sm={2}>
-                    1,110 HKD
-                   </Grid>
-                    <Grid item sm={2}>
-                    Fool-Club
-                   </Grid>
-                    <Grid item sm={1}>
-                    10/03/2018
-                   </Grid>
+                        <Grid item sm={2}>
+                            #190209-1B
+                        </Grid>
+                        <Grid item sm={2}>
+                            Success
+                            as of 10/17/18
+                        </Grid>
+                        <Grid item sm={2}>
+                            Tom cfsdaofo
+                        </Grid>
+                        <Grid item sm={2}>
+                            1,110 HKD
+                        </Grid>
+                        <Grid item sm={2}>
+                            Fool-Club
+                        </Grid>
                         <Grid item sm={1}>
-                    {this.state.open ? <ExpandLess/> : <ExpandMore/>}
+                            10/03/2018
+                        </Grid>
+                        <Grid item sm={1}>
+                            {this.state.open ? <ExpandLess/> : <ExpandMore/>}
                         </Grid>
                     </Grid>
                 </ListItem>
                 <Collapse in={this.state.open} timeout="auto" unmountOnExit>
                     <div button className={classes.nested}>
-                        <Grid container alignItems={'center'} spacing={16}>
-                            <Grid item sm={5}>
+                        <Grid container alignItems={'stretch'} spacing={40}
 
-                                <span className={'icon-shoes-24'}/>
-                                <span>Items</span>
-                                {
-                                    items.map((item, i) => {
-                                        return (
-                                            <div>
-                                             <span>
-                                        {item.name}(x{item.number})
-                                             </span>
-                                                <span className={'icon-barcode-16'}/>
-                                                <span>
+                        >
+                            <Grid item sm={5} className={classes.leftParent}>
+                                <Grid container alignItems={'flex-start '} className={classes.items}>
+                                    <Grid item>
+                                        <div className={'icon-phone-16' + ' ' + classes.detailIcon}/>
+                                    </Grid>
+
+                                    <Grid item sm={10}>
+                                        <span className={classes.detailHeader}>Items</span>
+                                        {
+                                            items.map((item, i) => {
+                                                return (
+                                                    <Grid container alignItems={'center'} justify={'space-between'}
+                                                    className={classes.detailPadding}>
+                                                        <Grid item>
+                                                            {item.name}(x{item.number})
+                                                        </Grid>
+                                                        <Grid item>
+                                                            <Grid container alignItems={'center'}>
+                                                            <span className={'icon-barcode-16'} style={{
+                                                                paddingRight: '10px',
+                                                            }}/>
+                                                                <span>
                                           {item.name}
                                                   </span>
-                                            </div>
+                                                            </Grid>
+                                                        </Grid>
+                                                    </Grid>
 
 
-                                        )
-                                    })
-                                }
-                                <Divider/>
-                                <span className={'icon-phone-16'}/>
-                                <span>
-        Contact Details
-    </span>
-                                <div>
-                                    +852 3829 1092
-                                </div>
-                                <div>
-                                    email@gmail.com
-                                </div>
+                                                )
+                                            })
+                                        }
+                                    </Grid>
+
+                                </Grid>
+                                <Grid container alignItems={'flex-start '} className={classes.contactDetail}>
+                                    <Grid item>
+                                        <div className={'icon-phone-16' + ' ' + classes.detailIcon}/>
+
+                                    </Grid>
+
+                                    <Grid item sm={10}>
+                                            <span className={classes.detailHeader}>
+                                                Contact Information
+                                            </span>
+                                        <div className={classes.detailContent +' '+classes.detailPadding}>
+                                            +852 3829 1092
+                                        </div>
+                                        <div className={classes.detailContent +' '+classes.detailPadding}>
+                                            email@gmail.com
+                                        </div>
+                                    </Grid>
+                                </Grid>
                             </Grid>
-                            <Grid item sm={7}>
-                                <Grid container alignItems={'center'}>
-                                    <Grid item sm={12}>
-                                        <span className={'icon-warning-16'}/>
-                                        <span>
+
+                            <Grid item sm={7}
+                                  className={classes.shippingDetails}
+                                  >
+                                <Grid container alignItems={'flex-start'} className={classes.rightChild}>
+                                    <Grid item>
+                                        <span className={'icon-warning-16' + ' ' + classes.detailIcon}/>
+                                    </Grid>
+                                    <Grid item sm={10} container>
+                                        <Grid item sm={12}>
+                                        <span className={classes.detailHeader}>
                         Shiping Details
                     </span>
-                                    </Grid>
+                                        </Grid>
 
-                                    <Grid itme sm={6}>
-                                        <div>
-                                            company
-                                        </div>
-                                        <div>
-                                            shipping cost
-                                        </div>
-                                        <div>
-                                            tracking details
-                                        </div>
-                                        <div>
-                                            delivery type
-                                        </div>
-                                        <div>
-                                            location
-                                        </div>
 
-                                    </Grid>
-                                    <Grid itme sm={6}>
-                                        <div>
-                                            SF Express
-                                        </div>
-                                        <div>
-                                            Free Shipping
-                                        </div>
-                                        <div>
-                                            www.thisissomelink.com/tracking
-                                        </div>
-                                        <div>
-                                            Premium Delivery
-                                        </div>
-                                        <div>
-                                            12D, Flr 5, Block 3, Ming Ting Crescent,
-                                            Tung Chung Lautau Island, Hong Kong
-                                        </div>
-                                    </Grid>
+                                        <Grid itme sm={12}>
+                                            <Grid container alignItems={'flex-start'} className={classes.detailPadding}>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        company
+                                                    </div>
+                                                </Grid>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        SF Express
+                                                    </div>
+                                                </Grid>
+                                            </Grid>
+                                            <Grid container alignItems={'flex-start'} className={classes.detailPadding}>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        shipping cost
+                                                    </div>
+                                                </Grid>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        Free Shipping
 
+                                                    </div>
+                                                </Grid>
+                                            </Grid>
+                                            <Grid container alignItems={'flex-start'} className={classes.detailPadding}>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        tracking details
+
+                                                    </div>
+                                                </Grid>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        www.thisissomelink.com/tracking
+
+                                                    </div>
+                                                </Grid>
+                                            </Grid>
+                                            <Grid container alignItems={'flex-start'} className={classes.detailPadding}>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        delivery type
+
+                                                    </div>
+                                                </Grid>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        Premium Delivery
+
+                                                    </div>
+                                                </Grid>
+                                            </Grid>
+
+                                            <Grid container alignItems={'flex-start'} className={classes.detailPadding}>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        location
+
+                                                    </div>
+                                                </Grid>
+                                                <Grid item sm={6}>
+                                                    <div>
+                                                        12D, Flr 5, Block 3, Ming Ting Crescent,
+                                                        Tung Chung Lautau Island, Hong Kong
+
+                                                    </div>
+                                                </Grid>
+                                            </Grid>
+
+                                        </Grid>
+                                    </Grid>
                                 </Grid>
                             </Grid>
                         </Grid>
